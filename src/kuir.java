5e971224db7bc6a456987932770b92079934d2f0
hwihwi99 <chamgrace@naver.com>
2022-03-30 09:47:05 +0900

    args에 따른 다른 실행되게끔 main함수 변경

--- src/kuir.java
+++ src/kuir.java
@@ -5,10 +5,20 @@ import javax.xml.transform.TransformerException;
 import java.io.IOException;
 
 public class kuir {
-    public static void main(String[] args) throws ParserConfigurationException, IOException, TransformerException, SAXException {
-        makeCollection xml = new makeCollection();
-        xml.run();
-        makeKeyword morphemeAnalysis = new makeKeyword();
-        morphemeAnalysis.morphemeAnalysis();
+    public static void main(String[] args) throws ParserConfigurationException, IOException, TransformerException, SAXException, ClassNotFoundException {
+
+        String command = args[0];
+        String path = args[1];
+
+        if(command.equals("-i")){
+            makeCollection xml = new makeCollection();
+            xml.run();
+        } else if(command.equals("-m")) {
+            makeKeyword morphemeAnalysis = new makeKeyword();
+            morphemeAnalysis.morphemeAnalysis();
+        }else if(command.equals("-d")) {
+            indexPost indexPost = new indexPost();
+            indexPost.makeIndexPost();
+        }
     }
 }
